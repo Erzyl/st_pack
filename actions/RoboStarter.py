2cc82f967c12c654ec930b7cb9c83581284e0517
Erzyl <emil.kammarbo@gmail.com>
2020-04-17 09:15:07 +0200

    first

--- actions/RoboStarter.py
+++ actions/RoboStarter.py
@@ -1,10 +1,13 @@
 import sys
+import webbrowser
 
 from st2common.runners.base_action import Action
 
 class RoboStarter(Action):
     def run(self, the_test):
         print(the_test)
+
+        webbrowser.open('http://net-informations.com', new=2)
 
         if the_test == 'working':
             return (True, the_test)
